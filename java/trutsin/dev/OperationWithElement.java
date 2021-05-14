acccfa547dca917d3214d349fdb5597e0a28b49f
Sergey Trutsin <sergey.trutsin@gmail.com>
2021-05-14 17:19:47 +0300

    change count of large degree

--- java/trutsin/dev/OperationWithElement.java
+++ java/trutsin/dev/OperationWithElement.java
@@ -65,15 +65,18 @@ public class OperationWithElement {
 
     public Element transformation_AChH_In_FNCh(Element element) {
         Element newElement = new Element();
+        double degree1 = 1;
+        double degree2 = 1;
+        double degree3 = 1;
+        double degree4 = 1;
 
 //        -----------------------------First part---------------------------------------------
 
         int newCountOfMultiplierFirst = element.getCoefficientFirst().size() / 2;
 
-        ArrayList<Double> arrayOfLargeMultiplierFirst = new ArrayList<>();
         for (int i = 0; i < element.getCoefficientFirst().size(); i += 2) {
             Double oldCoefficient = element.getCoefficientFirst().get(i);
-            arrayOfLargeMultiplierFirst.add(Math.pow(oldCoefficient, element.getCoefficientFirst().get(i+1)));
+            degree1 *= Math.pow(oldCoefficient, element.getCoefficientFirst().get(i + 1));
         }
 
 //              First coefficient
@@ -82,17 +85,16 @@ public class OperationWithElement {
             Double oldCoefficient = element.getCoefficientFirst().get(i);
             double newCoefficient = Math.pow(oldCoefficient, -1);
             intermediateArray1.add(newCoefficient);
-            intermediateArray1.add(element.getCoefficientFirst().get(i+1));
+            intermediateArray1.add(element.getCoefficientFirst().get(i + 1));
         }
         newElement.setCoefficientFirst(intermediateArray1);
 
 //        --------------------------------Second part-------------------------------------------
 
-        ArrayList<Double> arrayOfLargeMultiplierSecond = new ArrayList<>();
         for (int i = 0; i < element.getCoefficientFirst().size(); i += 3) {
             Double oldCoefficient = element.getCoefficientFirst().get(i);
-            arrayOfLargeMultiplierSecond.add(Math.pow(Math.pow(oldCoefficient, 2) +
-                    Math.pow(element.getCoefficientSecond().get(i+1), 2), element.getCoefficientSecond().get(i+2)));
+            degree2 *= Math.pow(Math.pow(oldCoefficient, 2) + Math.pow(element.getCoefficientSecond().get(i + 1), 2),
+                    element.getCoefficientSecond().get(i + 2));
         }
 
 
@@ -102,12 +104,12 @@ public class OperationWithElement {
         for (int i = 0; i < element.getCoefficientSecond().size(); i += 3) {
             Double oldCoefficient = element.getCoefficientSecond().get(i);
             double newCoefficientFirst = oldCoefficient *
-                    Math.pow(Math.pow(oldCoefficient, 2) + Math.pow(element.getCoefficientSecond().get(i+1), 2), -1);
+                    Math.pow(Math.pow(oldCoefficient, 2) + Math.pow(element.getCoefficientSecond().get(i + 1), 2), -1);
             intermediateArray2.add(newCoefficientFirst);
             double newCoefficientSecond = element.getCoefficientSecond().get(i+1) *
-                    Math.pow(Math.pow(oldCoefficient, 2) + Math.pow(element.getCoefficientSecond().get(i+1), 2), -1);
+                    Math.pow(Math.pow(oldCoefficient, 2) + Math.pow(element.getCoefficientSecond().get(i + 1), 2), -1);
             intermediateArray2.add(newCoefficientSecond);
-            intermediateArray2.add(element.getCoefficientSecond().get(i+2));
+            intermediateArray2.add(element.getCoefficientSecond().get(i + 2));
         }
         newElement.setCoefficientSecond(intermediateArray2);
 
@@ -117,14 +119,13 @@ public class OperationWithElement {
 
 //        ---------------------------Fourth part---------------------------------------------------------
 
-        ArrayList<Double> arrayOfLargeMultiplierFourth = new ArrayList<>();
         for (int i = 0; i < element.getCoefficientFourth().size(); i += 3) {
             Double oldCoefficient = element.getCoefficientFourth().get(i);
-            arrayOfLargeMultiplierFourth.add(Math.pow(Math.pow(oldCoefficient, 2) +
-                    Math.pow(element.getCoefficientFourth().get(i+1), 2), -element.getCoefficientFourth().get(i+2)));
+            degree4 *= Math.pow(Math.pow(oldCoefficient, 2) +
+                    Math.pow(element.getCoefficientFourth().get(i + 1), 2), -element.getCoefficientFourth().get(i + 2));
         }
 
-//                Second coefficient
+//                Fourth coefficient
         ArrayList<Double> intermediateArray4 = new ArrayList<>();
         for (int i = 0; i < element.getCoefficientFourth().size(); i += 3) {
             Double oldCoefficient = element.getCoefficientFourth().get(i);
